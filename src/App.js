6ec89c631cbd884379faf52157cdb7508d6c6939
manojsaikumar <manojsaikumar452@gmail.com>
2021-07-16 18:32:26 +0530

    test

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ const App = () => {
 
   return (
     <div>
-      <h1 className="title">Let's learn about helm</h1>
+      <h1 className="title">Let's learn about helm charts</h1>
 
       <SearchForm
         searchTerm={searchTerm}
